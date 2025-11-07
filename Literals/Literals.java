d8bda74d7b434cce305cba7cfdda0602a8ce6372
rishimanjunath15 <rishimanjunath15@gmail.com>
2025-11-07 18:58:32 +0530

    literals

---
+++ Literals/Literals.java
@@ -0,0 +1,25 @@
+// In Java, a Literal is value of boolean, numeric, character, or string data. Any constant value that can be assigned to the variable is called a literal. 
+
+// Here 100 is a constant/literal.
+// int x = 100; 
+
+
+
+class Literals {
+    public static void main(String[] args)
+    {
+          // decimal-form literal
+        int a = 101; 
+          // octal-form literal
+        int b = 0100; 
+        // Hexa-decimal form literal
+        int c = 0xFace; 
+          // Binary literal
+        int d = 0b1111; 
+      
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
