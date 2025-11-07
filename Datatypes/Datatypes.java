c758ad8f7ffa3ef30a8af6faa7ee027392a66c9e
rishimanjunath15 <rishimanjunath15@gmail.com>
2025-11-07 18:47:53 +0530

    datatypes

---
+++ Datatypes/Datatypes.java
@@ -0,0 +1,38 @@
+
+// Datatypes
+
+// Primitive Datatypes :- Integer     (byte,short,int,long)     (1byte,2byte,4byte,8byte)
+//                        Float       (float,double)            (4byte,8byte)
+//                        Character   (char)                    (2byte)
+//                        Boolean     (true,false)
+                        
+
+
+
+class Datatypes {
+    public static void main(String[] args) {
+    
+        byte by = 12;
+        short sh = 186;
+        int in = 237;
+        long lo = 757647l;
+
+
+        float fl = 12.456f;
+        double db = 76.7765;
+
+
+        char ch = 'h';
+
+        boolean b = true;
+
+
+
+
+        System.out.println("Integer:" + by + " " + sh + " " + in + " " + lo);
+        System.out.println("Float:" +fl + " " + db );
+        System.out.println("Character:" +ch);
+        System.out.println("Boolean: " +b);
+
+    }
+}
