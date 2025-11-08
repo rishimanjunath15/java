9ae8e08eb1e0f07f683e7fcb3e15ac90470945f7
rishimanjunath15 <rishimanjunath15@gmail.com>
2025-11-08 08:17:37 +0530

    if-else statements

---
+++ If-Else/If_Else.java
@@ -0,0 +1,31 @@
+
+
+class If_Else {
+    public static void main(String[] args) {
+        int a = 6;
+        int b =17;
+
+        if(a<b){
+            System.out.println("hello");
+        }
+
+
+        int x = 16;
+        int y = 78;
+        int z = 27;
+
+        if(x%2==0){
+            System.out.println("hello if else");
+        }else{
+            System.out.println("hello from ourside");
+        }
+
+        if(x%2==1){
+            System.out.println("hello if else");
+        }else if(y>z){
+            System.out.println("hello from ourside");
+        }else{
+            System.out.println("helooooooo");
+        }
+    }
+}
