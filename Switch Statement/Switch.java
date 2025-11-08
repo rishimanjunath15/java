9a873d3eea11b38f9adb090f25b50701634a4a48
rishimanjunath15 <rishimanjunath15@gmail.com>
2025-11-08 08:34:12 +0530

    switch statement

---
+++ Switch Statement/Switch.java
@@ -0,0 +1,66 @@
+
+
+class Switch{
+    public static void main(String[] args) {
+        
+        int n =2;
+
+        switch(n){
+            case 1:
+                System.out.println("Abhi");
+                break;
+            case 2:
+                System.out.println("bhuvan");
+                break;
+            case 3:
+                System.out.println("chaman");
+                break;
+            case 4:
+                System.out.println("druva");
+                break;
+            case 5:
+                System.out.println("eshwar");
+                break;
+            case 6:
+                System.out.println("fakhar");
+                break;
+            case 7:
+                System.out.println("govind");
+                break;
+
+            default:
+                System.out.println("enter the correct number");
+
+        }
+
+        int day =5;
+
+        switch(day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("enter the correct number");
+
+        }
+    }
+}
