83dd6e1d88b6657ca3d4f98f5ad6fac6514c12ae
rishimanjunath15 <rishimanjunath15@gmail.com>
2025-11-08 08:22:55 +0530

    ternary operator

---
+++ Ternary Operator/Ternary.java
@@ -0,0 +1,11 @@
+
+
+class Ternary{
+    public static void main(String[] args) {
+        int x = 76;
+
+
+        String result = x%2==1 ? "the reminder will be zero" : "it will not be zero";
+        System.out.println(result);
+    }
+}
