db49d6613285c15c8ee34d5b2098d23b800c1305
rishimanjunath15 <rishimanjunath15@gmail.com>
2025-11-09 11:16:01 +0530

    loops

---
+++ Loops/Loops.java
@@ -0,0 +1,47 @@
+
+// While Loops
+// class Loops{
+//     public static void main(String[] args) {
+//         int i = 1;
+//         while(i<=5)
+//         {
+//             System.out.println("Hiiii");
+//             int j = 1;
+//             while(j<=3){
+//                 System.out.println("Helloooo");
+//                 j++;
+//             }
+//             i++;
+//         }
+//         System.out.println("Helloooo from rishi");
+//     }
+// }
+
+
+
+// do-while
+// class Loops{
+//     public static void main(String[] args) {
+//         int i =3;
+//         do { 
+//             System.out.println("hello");
+//             i++;
+//         } while (i<=5);
+//     }
+// }
+
+
+
+// for-loops
+class Loops {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Day " +i);
+            for(int j =9;j<=15;j++){
+                System.out.println(j + "-" + (j+1));
+            }
+        }
+
+    }
+}
